5886a7828f363cdee047ff82537d23c7bff07785
silvianaim02 <silvianaim68@gmail.com>
2022-06-06 12:23:01 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useContext } from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Homepage from './components/homepage/HomePage';
+import LoginPage from './components/loginpage/LoginPage';
+import Navbar from './components/navbar/Navbar';
+import { myContext } from './Context';
 
 function App() {
+  const userObject = useContext(myContext);
+  console.log(userObject)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          {
+            userObject ? null : (
+              <Route path='/login' element={<LoginPage />} />
+            )
+          }
+        </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/loginpage/LoginPage.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from './LoginPage.module.css'
+import googleImage from '../../assets/googleImage.png'
+import githubImage from '../../assets/githubImage.png'
+import twitterImage from '../../assets/twitterImage.png'
+
+const LoginPage = () => {
+    const googleLogin = () => {
+        window.open("https://passport-deploy.herokuapp.com/auth/google", "_self");
+    }
+
+
+    return (
+        <div className={styles.loginPage}>
+            <div className={styles.loginForm}>
+                <h1>Login</h1>
+                <div className={styles.googleContainer} onClick={googleLogin}>
+                    <img src={googleImage} alt="Google Icon" />
+                    <p>Login With Google</p>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default LoginPage
